f0cd929403f6f70ff12dce0c154dcd4783a99cf0
Sri Harish <test@L-MAA-25004247.paypalcorp.com>
2015-07-30 19:30:35 +0530

    Changed isSequence to getNextSequence

--- Hand.java
+++ Hand.java
@@ -3,11 +3,13 @@ package Rummy;
 import java.util.ArrayList;
 
 public class Hand{
-	ArrayList<Card> cards=null;
+	private ArrayList<Card> cards=null;
+	private int numberOfCards;
 
 	public Hand(ArrayList<Card> cards) {
 		super();
 		this.cards = cards;
+		numberOfCards = cards.size();
 	}
 	
 	public boolean isRummy(){
@@ -15,12 +17,28 @@ public class Hand{
 		
 	}
 	public int numberOfcardsForRummy() {
+		for (int i = 0; i < numberOfCards - 1; i += getNextSequence(i)) {
+			
+		}
 		return 0;	
 	}
 	
-	private int isSequence() {
-		
+	private int getNextSequence(int index) {
+		int number_of_cards = 1;
+		for(int i = index ; i < numberOfCards - 1; i++) {
+			Card current_card = cards.get(i);
+			if (current_card.isPrevious(nextCard(i))) {
+				number_of_cards++;
+			}
+		}
+		if (number_of_cards > 2) {
+			return index;
+		}
 		return 0;
+	}
+	
+	private Card nextCard(int index) {
+		return cards.get(index + 1);
 	}
 	
 }
